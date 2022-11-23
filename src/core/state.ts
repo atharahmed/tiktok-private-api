215039eb5dd689eba243c6967a9d431a10ca6cfb
Athar Ahmed <itsatharahmed@gmail.com>
2022-11-24 00:34:17 +0500

    allow overriding headers #3

--- src/core/state.ts
+++ src/core/state.ts
@@ -5,7 +5,24 @@ export class State {
   private _mobileUserAgent =
     "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1";
 
-  private _defaultApiHeaders = {
+  private _defaultHeaders: any = {
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-ch-ua":
+      '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": this._webUserAgent,
+  };
+
+  private _defaultApiHeaders: any = {
     accept: "application/json, text/plain, */*",
     "accept-encoding": "gzip",
     "accept-language": "en-US,en;q=0.9",
@@ -15,6 +32,7 @@ export class State {
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "none",
     "sec-gpc": "1",
+    "user-agent": this._webUserAgent,
   };
 
   public set mobileUserAgent(mobileUserAgent: string) {
@@ -31,6 +49,18 @@ export class State {
 
   public get webUserAgent() {
     return this._webUserAgent;
+  }
+
+  public set defaultHeaders(headers: any) {
+    this._defaultHeaders = headers;
+  }
+
+  public get defaultHeaders() {
+    return this._defaultHeaders;
+  }
+
+  public set defaultApiHeaders(headers: any) {
+    this._defaultApiHeaders = headers;
   }
 
   public get defaultApiHeaders() {
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -42,23 +42,10 @@ export class UserRepository extends Repository {
 
     this.userDebug(`Sending request to ${url}`);
 
-    const headers = {
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-encoding": "gzip, deflate, br",
-      "accept-language": "en-US,en;q=0.9",
-      "sec-ch-ua":
-        '" Not A;Brand";v="99", "Chromium";v="103", "Google Chrome";v="103"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"macOS"',
-      "sec-fetch-dest": "document",
-      "sec-fetch-mode": "navigate",
-      "sec-fetch-site": "none",
-      "sec-fetch-user": "?1",
-      "upgrade-insecure-requests": "1",
-      "user-agent": this.client.state.webUserAgent,
-    };
-    const response = await this.client.request.send(url, headers);
+    const response = await this.client.request.send(
+      url,
+      this.client.state.defaultHeaders
+    );
 
     const parsed = await this.parseHtmlContent(response.body);
 
@@ -138,7 +125,6 @@ export class UserRepository extends Repository {
       "https://www.tiktok.com/api/post/item_list/?aid=1988&app_language=en&app_name=tiktok_web&battery_info=1&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F107.0.0.0%20Safari%2F537.36%20Edg%2F107.0.1418.35&channel=tiktok_web&cookie_enabled=true&device_id=7002566096994190854&device_platform=web_pc&focus_state=false&from_page=user&history_len=3&is_fullscreen=false&is_page_visible=true&os=windows&priority_region=RO&referer=https%3A%2F%2Fexportcomments.com%2F&region=RO&root_referer=https%3A%2F%2Fexportcomments.com%2F&screen_height=1440&screen_width=2560&tz_name=Europe%2FBucharest&verifyFp=verify_lacphy8d_z2ux9idt_xdmu_4gKb_9nng_NNTTTvsFS8ao&webcast_language=en&msToken=7UfjxOYL5mVC8QFOKQRhmLR3pCjoxewuwxtfFIcPweqC05Q6C_qjW-5Ba6_fE5-fkZc0wkLSWaaesA4CZ0LAqRrXSL8b88jGvEjbZPwLIPnHeyQq6VifzyKf5oGCQNw_W4Xq12Q-8KCuyiKGLOw=&X-Bogus=DFSzswVL-XGANHVWS0OnS2XyYJUm&_signature=_02B4Z6wo00001Pf0DlwAAIDB1FUg8jgaqOz39ArAAF6Z72",
       {
         ...this.client.state.defaultApiHeaders,
-        "user-agent": this.client.state.webUserAgent,
         "x-tt-params": xTTParams,
       }
     );
@@ -197,7 +183,6 @@ export class UserRepository extends Repository {
 
     const response = await this.client.request.send(url.toString(), {
       ...this.client.state.defaultApiHeaders,
-      "user-agent": this.client.state.mobileUserAgent,
       "x-tt-params": xTTParams,
     });
 
